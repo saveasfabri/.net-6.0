8132877670f62da711da0af593db90a03eea36f2
Fabricio Hang <saveasfabri@gmail.com>
2022-05-19 07:11:06 -0300

    Actualiza carpetas

---
+++ clase-07/Tarea clase 7/Tarea clase 7/Program.cs
@@ -0,0 +1,22 @@
+﻿//Tarea clase 07
+
+//Escribe un programa que:
+
+//1) Pida 10 números al usuario.
+//2) Obtener la suma de todos los números ingresados.
+//3) Obtener cual es el mayor de todos los números.
+//4) Obtener cual es el menor de todos los números.
+//5) Obtener el promedio de todos los números.
+//6) Mostrar todos los números ingresados por pantalla.
+//7) Mostrar los valores calculados en los puntos 2, 3, 4 y 5.
+
+//****************************************************************************
+
+Console.WriteLine("Hola, ¡bienvenido a este coso!");
+
+for (int i = 0; i < 10; i++)
+{
+  Console.WriteLine("Ingrese un número");
+  int numero = int.Parse(Console.ReadLine());
+
+}
